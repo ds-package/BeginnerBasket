3bc31af4ed085dc211fcf03dc184e070511bbde4
minhee <mini940511@gmail.com>
2022-11-22 03:41:26 +0900

    08 - 메뉴필터
    ---
    
    - dataset
    - map
    - join
    - innerHTML
    - filter
    뭔가 사용한 것들은 많은데 이번엔 진짜 따라가기 급급했다.
    
    너무 어려움!

---
+++ 08-menu/app.js
@@ -0,0 +1,105 @@
+const menu = [
+  {
+    id: 1,
+    title: "mini",
+    category: "cute",
+    price: 15.99,
+    img: "/assets/img-01.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "juni",
+    category: "cute",
+    price: 13.99,
+    img: "/assets/img-02.jpg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "dahee",
+    category: "cool",
+    price: 6.99,
+    img: "/assets/img-03.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "woona",
+    category: "hot",
+    price: 20.99,
+    img: "/assets/img-04.jpg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "johell",
+    category: "cool",
+    price: 22.99,
+    img: "/assets/img-05.jpg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "kooooooog",
+    category: "hot",
+    price: 18.99,
+    img: "/assets/img-06.jpg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+];
+
+const sectionCenter = document.querySelector(".section-center");
+
+// 필터
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+// 아이템 불러오기
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+});
+
+// 필터 아이템
+// 와 여기는 치면서도 정말 잘 모르겠다, 해석이 필요함
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      // console.log(menuItem.category);
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+
+    // console.log(menuCategory);
+    // 카테고리가 "all"이 아니라면 "menuCategory"를 불러와?
+    if (category === "all") {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} class="photo" alt=${item.title} />
+          <div class="item-info">
+            <header>
+              <h3>${item.title}</h3>
+              <h3 class="price">${item.price}</h3>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+
+  // 이걸 추가하니 데이터로 작성되어있는 메뉴가 쭉 나열되었는데 왜 됐는지 검색이 필요함
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
